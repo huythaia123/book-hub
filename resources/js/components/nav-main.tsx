51d9aed04859e10657cf8426f8040087a66cebad
huy thai <77420642+huythaia123@users.noreply.github.com>
2025-09-29 11:53:48 +0700

    Refactor AppSidebar and NavMain components for improved sidebar functionality and styling; update Sidebar variant and enhance item mapping logic with active state handling.

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -10,24 +10,34 @@ import { Link, usePage } from '@inertiajs/react';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
+
     return (
         <SidebarGroup className='px-2 py-0'>
             <SidebarGroupLabel>Platform</SidebarGroupLabel>
             <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton
-                            asChild
-                            isActive={page.url.startsWith(typeof item.href === 'string' ? item.href : item.href.url)}
-                            tooltip={{ children: item.title }}
-                        >
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
+                {items.map((item) => {
+                    const itemUrl = typeof item.href === 'string' ? item.href : item.href.url;
+                    const pageUrl = page.url.split('?')[0];
+                    const isActive = itemUrl === pageUrl;
+                    // console.log('itemUrl', itemUrl, 'pageUrl', pageUrl);
+                    // console.log(itemUrl, isActive);
+
+                    return (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                asChild
+                                isActive={isActive}
+                                tooltip={{ children: item.title }}
+                                className={`data-[active=true]:bg-blue-500 data-[active=true]:text-white`}
+                            >
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    );
+                })}
             </SidebarMenu>
         </SidebarGroup>
     );
