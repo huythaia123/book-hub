6c95a2392af949ca52416431c65865204f4d4f1b
huy thai <77420642+huythaia123@users.noreply.github.com>
2025-10-06 17:15:56 +0700

    Remove commented-out code from RoleUserSeeder and UserSeeder for clarity and maintainability.

--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -15,19 +15,6 @@ class RoleUserSeeder extends Seeder
      */
     public function run(): void
     {
-        // $userAdmin = User::first();
-        // if ($userAdmin) {
-        //     $adminRole = Role::where('name', RoleEnum::Admin->value)->first();
-        //     $userAdmin->roles()->syncWithoutDetaching([$adminRole->id]);
-        // }
-
-        // Random gán role cho 10 user đầu tiên
-        // $users = User::take(10)->get();
-        // foreach ($users as $user) {
-        //     $roleIds = Role::inRandomOrder()->take(rand(1, 2))->pluck('id');
-        //     $user->roles()->syncWithoutDetaching($roleIds);
-        // }
-
         for ($i = 0; $i < count(RoleEnum::values()); $i++) {
             $email = strtolower(RoleEnum::values()[$i]) . '@example.com';
             $user = User::where('email', $email)->first();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,19 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::firstOrCreate(
-        //     ['email' => env('SEED_USER_EMAIL')],
-        //     [
-        //         'name' => env('SEED_USER_NAME'),
-        //         'password' => Hash::make(env('SEED_USER_PASSWORD')),
-        //         'email_verified_at' => now(),
-        //         'created_at' => now(),
-        //         'updated_at' => now()
-        //     ]
-        // );
-
-        // User::factory(9)->create();
-
         for ($i = 0; $i < count(RoleEnum::values()); $i++) {
             User::firstOrCreate(
                 ['email' => strtolower(RoleEnum::values()[$i]) . '@example.com'],
