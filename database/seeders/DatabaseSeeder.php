3825bbf943ab3e7e23bae09f4fd0b283a2ade780
huy thai <77420642+huythaia123@users.noreply.github.com>
2025-10-08 11:15:16 +0700

    Implement CRUD functionality for Chapter resource, including controller, model, policy, migration, and seeder; update database seeder and todo list.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             RoleUserSeeder::class,
             BookSeeder::class,
+            ChapterSeeder::class,
         ]);
     }
 }
